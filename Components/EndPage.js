c7ad0e193c7d76845b93189a837bace5a9137cd1
athdromeda <anixatho01@gmail.com>
2022-10-27 06:57:42 +0700

    style: apply background image

--- Components/EndPage.js
+++ Components/EndPage.js
@@ -1,17 +1,42 @@
+import Image from "next/image";
+import { useState } from "react";
 import { Zoom } from "react-reveal";
 
 export default function EndPage() {
+  const [isHover, setHover] = useState(false);
+
+  const handleMouseOver = () => {
+    setHover(true);
+  };
+  
+  const handleMouseOut = () => {
+    setHover(false);
+  };
+
   return (
-    <div className="h-[80vh] py-20 px-16 bg-white text-dark flex flex-col items-center justify-around">
-      <Zoom>
-        <p className="text-4xl font-bold text-center">
-          Mulai belajar dan arungi luasnya keilmuan Islam!
-        </p>
-        <button className="font-bold bg-primary text-white px-8 py-4 rounded-lg">
-          Daftar Sekarang
-        </button>
-        <section className="flex"></section>
-      </Zoom>
+    <div className="h-[80vh] py-20 px-16 bg-white text-dark relative">
+      <div className={(isHover? 'opacity-10':'opacity-20')}>
+        <Image
+          src="/assets/end-page.jpg"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-evenly h-full">
+        <Zoom>
+          <p className="text-4xl font-bold text-center z-20">
+            Mulai belajar dan arungi luasnya keilmuan Islam!
+          </p>
+          <button
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+            className="font-bold bg-primary text-white px-8 py-4 rounded-lg z-20"
+          >
+            Daftar Sekarang
+          </button>
+        </Zoom>
+      </div>
     </div>
   );
 }
